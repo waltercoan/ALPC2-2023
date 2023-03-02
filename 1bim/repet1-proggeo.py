0914943ffc172575e8b87c98e75beaa746570d50
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-03-02 20:41:12 -0300

    02/03/2023 - revisao blocos de repeticao

---
+++ 1bim/repet1-proggeo.py
@@ -0,0 +1,33 @@
+
+##qqcoisa = False
+#while qqcoisa == False:
+#contador = 0
+#while contador < 200:
+#    print(contador)
+#    contador += 1
+    #contador = contador +  1
+
+#print(range(200))
+#print(list(range(200)))
+result = 1
+for contador in range(1,201):
+    if contador % 4 == 0:
+        result *= contador
+print(result)
+
+
+
+
+
+'''
+if True:
+    print("Verdade")
+else:
+    if False: 
+        print("Falso")
+
+if True:
+    print("Verdade")
+elif False:
+    print("Falso")
+'''
