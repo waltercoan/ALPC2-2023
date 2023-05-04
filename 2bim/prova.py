b2c6bd889ba330a1db4459adc8321a30ccbd6a41
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-05-03 21:09:58 -0300

    menu

---
+++ 2bim/prova.py
@@ -0,0 +1,15 @@
+opcao = 0
+while opcao != 4:
+    print("Menu")
+    print("1 - Opção 1")
+    print("2 - Opção 2")
+    print("3 - Opção 3")
+    print("4 - SAIR")
+    print("Digite a opcao desejada")
+    opcao = int(input())
+    if opcao == 1:
+        print("Codigo da opcao1")
+    if opcao == 2:
+        print("Codigo da opcao2")
+    if opcao == 3:
+        print("Codigo da opcao3")
